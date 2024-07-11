51ba89330dd3938c9b055115058672fb7b4a3e79
gaehyun <gaehyunee@gmail.com>
2024-07-11 20:48:10 +0900

    [추가] search.dart 박스 알고리즘 수정

--- lib/src/pages/search.dart
+++ lib/src/pages/search.dart
@@ -1,4 +1,9 @@
+import 'dart:math';
+
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:quiver/iterables.dart';
 
 class Search extends StatefulWidget {
   const Search({Key? key}) : super(key: key);
@@ -9,14 +14,20 @@ class Search extends StatefulWidget {
 
 class _SearchState extends State<Search> {
   List<List<int>> groupBox = [[], [], []];
+  List<int> groupIndex = [0, 0, 0]; //매칭되는 위치에 더해주기
 
   @override
   void initState() {
     super.initState();
     for (var i = 0; i < 100; i++) {
-      groupBox[i % 3].add(1);
+      var gi = groupIndex.indexOf(min<int>(groupIndex)!);
+      var size = 1;
+      if (gi != 1) {
+        size = Random().nextInt(100) % 2 == 0 ? 1 : 2;
+      }
+      groupBox[gi].add(size);
+      groupIndex[gi] += size;
     }
-    print(groupBox);
   }
 
   Widget _appbar() {
@@ -57,70 +68,27 @@ class _SearchState extends State<Search> {
       child: Row(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: List.generate(
-            groupBox.length,
-            (index) => Expanded(
-                  child: Column(
-                    children: [
-                      Container(
-                        height: 280,
-                        color: Colors.red,
-                      ),
-                      Container(
-                        height: 140,
-                        color: Colors.purple,
-                      ),
-                    ],
+          groupBox.length,
+          (index) => Expanded(
+            child: Column(
+              children: List.generate(
+                groupBox[index].length,
+                (jndex) => Container(
+                  height: Get.width * 0.33 * groupBox[index][jndex],
+                  decoration: BoxDecoration(
+                      border: Border.all(color: Colors.white),
+                      color: Colors.primaries[
+                          Random().nextInt(Colors.primaries.length)]),
+                  child: CachedNetworkImage(
+                    imageUrl:
+                        'https://picsum.photos/200/300?random=${Random().nextInt(100)}',
+                    fit: BoxFit.cover,
                   ),
-                )).toList(),
-
-        // children: [
-        //   Expanded(
-        //     child: Column(
-        //       children: [
-        //         Container(
-        //           height: 280,
-        //           color: Colors.red,
-        //         ),
-        //         Container(
-        //           height: 140,
-        //           color: Colors.purple,
-        //         ),
-        //       ],
-        //     ),
-        //   ),
-        //   Expanded(
-        //     child: Column(
-        //       children: [
-        //         Container(
-        //           height: 140,
-        //           color: Colors.orange,
-        //         ),
-        //         Container(
-        //           height: 140,
-        //           color: Colors.blue,
-        //         ),
-        //       ],
-        //     ),
-        //   ),
-        //   Expanded(
-        //     child: Column(
-        //       children: [
-        //         Container(
-        //           height: 140,
-        //           color: Colors.blue,
-        //         ),
-        //         Container(
-        //           height: 280,
-        //           color: Colors.yellow,
-        //         ),
-        //         Container(
-        //           height: 140,
-        //           color: Colors.grey,
-        //         ),
-        //       ],
-        //     ),
-        //   ),
-        // ],
+                ),
+              ).toList(),
+            ),
+          ),
+        ).toList(),
       ),
     );
   }
